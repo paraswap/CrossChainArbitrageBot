4ebb2c69a8ffe06c5526fb179def6693a31cf5c2
Shresth Agrawal <shresthagrawal.31@gmail.com>
2021-06-16 20:51:16 +0530

    Fix CrossChainArbinator

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 
-const { ethers } = require("ethers");
-const bigNumber = require("bignumber.js");
-const Paraswap = require("./pricing/paraswap");
+const { ethers } = require('ethers');
+const BigNumber = require('bignumber.js');
+const Paraswap = require('./paraswap');
 
 const REST_TIME = 5 * 1000; // 5 seconds
 const MAINNET_NETWORK_ID = 1;
 const POLYGON_NETWORK_ID = 137;
-const slippage = 0.05;
+const slippage = 0.03;
 
 const providerURLs = {
   [MAINNET_NETWORK_ID]: process.env.HTTP_PROVIDER_MAINNET,
@@ -23,21 +23,21 @@ const privatekey = {
 const Tokens = {
   [MAINNET_NETWORK_ID]: {
     ETH: {
-      address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+      address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
       decimals: 18,
     },
     MATIC: {
-      address: "0xa0c68c638235ee32657e8f720a23cec1bfc77c77",
+      address: '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0',
       decimals: 18,
     },
   },
   [POLYGON_NETWORK_ID]: {
     MATIC: {
-      address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+      address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
       decimals: 18,
     },
-    WETH: {
-      address: "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
+    ETH: {
+      address: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', // Its actually WETH
       decimals: 18,
     },
   },
@@ -50,53 +50,76 @@ class CrossChainArbinator {
   }
 
   async alive() {
-    await this.run();
-    await this.alive();
+    try {
+      await this.run();
+    } catch (e) {
+      console.error(`Error_CrossChainArbinator_alive:`, e);
+    }
+    return await this.alive();
   }
 
   executeTx(txRequest, network) {
-    return this.wallets[network].sendTransaction(tx);
+    return this.wallets[network].sendTransaction(txRequest);
   }
 
   async rebalance() {
     // TODO: complete me
   }
 
+  normalise(amount, token) {
+    return new BigNumber(amount).times(new BigNumber(10).pow(token.decimals));
+  }
+
+  denormalise(amount, token) {
+    return new BigNumber(amount).div(new BigNumber(10).pow(token.decimals));
+  }
+
   // Bot logic goes here
   async run() {
-    const srcAmountFirst = new bigNumber("10000000000000000"); // 0.1 ETH
+    const srcAmountFirst = this.normalise(
+      '0.1',
+      Tokens[MAINNET_NETWORK_ID]['ETH'],
+    );
     const priceFirst = await this.pricing.getPrice(
-      Tokens[MAINNET_NETWORK_ID]["ETH"].address,
-      Tokens[MAINNET_NETWORK_ID]["ETH"].decimals,
-      Tokens[MAINNET_NETWORK_ID]["MATIC"].address,
-      Tokens[MAINNET_NETWORK_ID]["MATIC"].decimals,
+      Tokens[MAINNET_NETWORK_ID]['ETH'],
+      Tokens[MAINNET_NETWORK_ID]['MATIC'],
       srcAmountFirst.toFixed(0),
-      MAINNET_NETWORK_ID
+      MAINNET_NETWORK_ID,
     );
+    const dSrcAmountFirst = this.denormalise(
+      srcAmountFirst,
+      Tokens[MAINNET_NETWORK_ID]['ETH'],
+    ).toFixed(4);
+    const dDestAmountFirst = this.denormalise(
+      priceFirst.price,
+      Tokens[MAINNET_NETWORK_ID]['MATIC'],
+    ).toFixed(4);
     console.log(
-      `FirstSwap ETH -> MATIC MAINNET srcAmount: ${srcAmountFirst.toFixed(
-        0
-      )} destAmount: ${priceFirst.price}`
+      `FirstSwap ETH -> MATIC MAINNET srcAmount: ${dSrcAmountFirst} destAmount: ${dDestAmountFirst}`,
     );
-    const destAmountFirstSlippage = new bignumber(priceFirst.price).times(
-      1 - slippage
+    const destAmountFirstSlippage = new BigNumber(priceFirst.price).times(
+      1 - slippage,
     );
 
     const priceSecond = await this.pricing.getPrice(
-      Tokens[POLYGON_NETWORK_ID]["MATIC"].address,
-      Tokens[POLYGON_NETWORK_ID]["MATIC"].decimals,
-      Tokens[POLYGON_NETWORK_ID]["ETH"].address,
-      Tokens[POLYGON_NETWORK_ID]["ETH"].decimals,
+      Tokens[POLYGON_NETWORK_ID]['MATIC'],
+      Tokens[POLYGON_NETWORK_ID]['ETH'],
       destAmountFirstSlippage.toFixed(0),
-      POLYGON_NETWORK_ID
+      POLYGON_NETWORK_ID,
     );
+    const dSrcAmountSecond = this.denormalise(
+      destAmountFirstSlippage,
+      Tokens[POLYGON_NETWORK_ID]['MATIC'],
+    ).toFixed(4);
+    const dDestAmountSecond = this.denormalise(
+      priceSecond.price,
+      Tokens[POLYGON_NETWORK_ID]['ETH'],
+    ).toFixed(4);
     console.log(
-      `SecondSwap MATIC -> ETH MAINNET srcAmount: ${destAmountFirstSlippage.toFixed(
-        0
-      )} destAmount: ${priceSecond.price}`
+      `SecondSwap MATIC -> ETH MAINNET srcAmount: ${dSrcAmountSecond} destAmount: ${dDestAmountSecond}`,
     );
     const destAmountSecondSlippage = new BigNumber(priceSecond.price).times(
-      1 - slippage
+      1 - slippage,
     );
 
     const isArb = srcAmountFirst.lte(destAmountSecondSlippage);
@@ -105,40 +128,36 @@ class CrossChainArbinator {
       const [txRequestMainnet, txRequestPolygon] = await Promise.all([
         this.pricing.buildTransaction(
           priceFirst.payload,
-          Tokens[MAINNET_NETWORK_ID]["ETH"].address,
-          Tokens[MAINNET_NETWORK_ID]["ETH"].decimals,
-          Tokens[MAINNET_NETWORK_ID]["MATIC"].address,
-          Tokens[MAINNET_NETWORK_ID]["MATIC"].decimals,
+          Tokens[MAINNET_NETWORK_ID]['ETH'],
+          Tokens[MAINNET_NETWORK_ID]['MATIC'],
           srcAmountFirst.toFixed(0),
           destAmountFirstSlippage.toFixed(0),
           MAINNET_NETWORK_ID,
-          this.wallets[MAINNET_NETWORK_ID].address
+          this.wallets[MAINNET_NETWORK_ID].address,
         ),
         this.pricing.buildTransaction(
           priceSecond.payload,
-          Tokens[POLYGON_NETWORK_ID]["MATIC"].address,
-          Tokens[POLYGON_NETWORK_ID]["MATIC"].decimals,
-          Tokens[POLYGON_NETWORK_ID]["ETH"].address,
-          Tokens[POLYGON_NETWORK_ID]["ETH"].decimals,
+          Tokens[POLYGON_NETWORK_ID]['MATIC'],
+          Tokens[POLYGON_NETWORK_ID]['ETH'],
           destAmountFirstSlippage.toFixed(0),
           destAmountSecondSlippage.toFixed(0),
           POLYGON_NETWORK_ID,
-          this.wallets[POLYGON_NETWORK_ID].address
+          this.wallets[POLYGON_NETWORK_ID].address,
         ),
       ]);
-
+      console.log('Executing Arbitrage');
       // TODO: handle failed tx
       const txs = await Promise.all([
-        this.executeTx.executeTx(txRequestMainnet, MAINNET_NETWORK_ID),
-        this.executeTx.executeTx(txRequestPolygon, POLYGON_NETWORK_ID),
+        this.executeTx(txRequestMainnet, MAINNET_NETWORK_ID),
+        this.executeTx(txRequestPolygon, POLYGON_NETWORK_ID),
       ]);
 
       console.log(txs);
       await this.rebalance();
     } else {
       // Take Rest
-      await Promise((resolve) => {
-        setTimeout(() => resolve, REST_TIME);
+      await new Promise(resolve => {
+        setTimeout(() => resolve(), REST_TIME);
       });
     }
   }
@@ -147,20 +166,20 @@ class CrossChainArbinator {
 async function main() {
   const providers = {
     [MAINNET_NETWORK_ID]: new ethers.providers.JsonRpcProvider(
-      providerURLs[MAINNET_NETWORK_ID]
+      providerURLs[MAINNET_NETWORK_ID],
     ),
     [POLYGON_NETWORK_ID]: new ethers.providers.JsonRpcProvider(
-      providerURLs[POLYGON_NETWORK_ID]
+      providerURLs[POLYGON_NETWORK_ID],
     ),
   };
   const wallets = {
     [MAINNET_NETWORK_ID]: new ethers.Wallet(
       privatekey[MAINNET_NETWORK_ID],
-      providers[MAINNET_NETWORK_ID]
+      providers[MAINNET_NETWORK_ID],
     ),
     [POLYGON_NETWORK_ID]: new ethers.Wallet(
       privatekey[POLYGON_NETWORK_ID],
-      providers[POLYGON_NETWORK_ID]
+      providers[POLYGON_NETWORK_ID],
     ),
   };
 
--- paraswap.js
+++ paraswap.js
@@ -1,55 +1,67 @@
-const axios = require("axios");
+const axios = require('axios');
 
-const ParswapURL = "https://apiv4.paraswap.io/v2";
+const ParswapURL = 'https://apiv4.paraswap.io/v2';
 
 // https://developers.paraswap.network/
 class Paraswap {
   constructor(apiURL = ParswapURL) {
     this.apiURL = apiURL;
-    this.referrer = "arb-bot";
+    this.referrer = 'arb-bot';
   }
 
-  async getPrice(from, fromDecimals, to, toDecimals, srcAmount, network) {
+  async getPrice(from, to, srcAmount, network) {
     // TODO: Add error handling
-    const requestURL = `${this.apiURL}/prices/?from=${from}&to=${to}&amount=${srcAmount}$&fromDecimals=${fromDecimals}&toDecimals=${toDecimals}&side=SELL&network=${network}`;
-    const { data } = await axios.get(requestURL, {
-      headers: {
-        "X-Partner": referrer,
-      },
-    });
-    return {
-      price: data.priceRoute.destAmount,
-      payload: data.priceRoute,
-    };
+    try {
+      const requestURL =
+        `${this.apiURL}/prices/?from=${from.address}&to=${to.address}` +
+        `&amount=${srcAmount}&fromDecimals=${from.decimals}&toDecimals` +
+        `=${to.decimals}&side=SELL&network=${network}`;
+      const { data } = await axios.get(requestURL, {
+        headers: {
+          'X-Partner': this.referrer,
+        },
+      });
+      return {
+        price: data.priceRoute.destAmount,
+        payload: data.priceRoute,
+      };
+    } catch (e) {
+      throw new Error(
+        `Paraswap unable to fetch price ${from} ${to} ${network} ${e.message}`,
+      );
+    }
   }
 
   async buildTransaction(
     pricePayload,
     from,
-    fromDecimals,
     to,
-    toDecimals,
     srcAmount,
     minDestAmount,
     network,
-    userAddress
+    userAddress,
   ) {
-    // TODO: Add error handling
-    const requestURL = `${this.apiURL}/transactions/${network}`;
-    const requestData = {
-      priceRoute: pricePayload,
-      srcToken: from,
-      destToken: to,
-      srcAmount: srcAmount,
-      destAmount: minDestAmount,
-      userAddress: userAddress,
-      referrer: this.referrer,
-      srcDecimals: fromDecimals,
-      destDecimals: toDecimals,
-    };
+    try {
+      const requestURL = `${this.apiURL}/transactions/${network}?skipChecks=true`;
+      const requestData = {
+        priceRoute: pricePayload,
+        srcToken: from.address,
+        destToken: to.address,
+        srcAmount: srcAmount,
+        destAmount: minDestAmount,
+        userAddress: userAddress,
+        referrer: this.referrer,
+        srcDecimals: from.decimals,
+        destDecimals: to.decimals,
+      };
 
-    const { data } = await axios.post(txURL, requestData);
-    return data;
+      const { data } = await axios.post(requestURL, requestData);
+      return data;
+    } catch (e) {
+      throw new Error(
+        `Paraswap unable to buildTransaction ${from} ${to} ${network} ${e.message}`,
+      );
+    }
   }
 }
 
